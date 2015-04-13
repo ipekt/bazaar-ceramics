f858b2009737e7d7a70288d8583dbc5443999409
Ipek Turgul <ipek.turgul@gmail.com>
2015-04-13 21:02:36 +1000

    push menu style

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -2,25 +2,25 @@
 //= require jquery-2.1.3.min
 
 var main = function() {
-  /* Push the body and the nav over by 285px over */
+  /* Push the body and the nav over by 400px over */
   $('.icon-menu').click(function() {
     $('.menu').animate({
       left: "0px"
     }, 200);
 
     $('body').animate({
-      left: "60%"
+      left: "400px"
     }, 200);
   });
 
   /* Then push them back */
   $('.icon-close').click(function() {
     $('.menu').animate({
-      left: "-60%"
+      left: "-400px"
     }, 200);
 
     $('body').animate({
-      left: "0px"
+      left: "0px",
     }, 200);
   });
 };
